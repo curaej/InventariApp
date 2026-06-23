207a491f73376cb194766ca913bcfef91493e60a
curaej <curaelnub@gmail.com>
2026-06-23 10:09:24 -0600

    Primer push con estructurabase de proyecto y configuración de Gitignore

---
+++ src/Program.cs
@@ -0,0 +1,15 @@
+﻿//
+using System.Reflection;
+
+var asembly = Assembly.GetExecutingAssembly();
+var version = asembly.GetName().Version;
+
+Console.WriteLine("                         ");
+Console.WriteLine("|| Sistema de gestión de Inventario ||");
+Console.WriteLine();
+Console.WriteLine($"Versión 1.0.0");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".NET Versión: {Environment.Version}");
+Console.WriteLine();
+Console.WriteLine("Estado: Proyecto Inicializado");
+Console.WriteLine($" Version: {version}");
